1df0a1220933bfe655a9cee49876f527317f4707
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-11-13 16:10:41 +0100

    &&-bug

--- react-props/react-props/src/components/AppMain.jsx
+++ react-props/react-props/src/components/AppMain.jsx
@@ -2,14 +2,18 @@ import PostCard from "./PostCard"
 import Posts from "../data/Posts"
 
 export default function AppMain() {
-
-
     return (
-        Posts.map((post, index) => {
-            post.published === "true" &&
-                <div className="container">
-                    <PostCard key={index} title={post.title} image={post.image} content={post.content} tags={post.tags} />
+        Posts.map((post, index) => (
+            post.published === "true" && (
+                <div key={index} className="container">
+                    <PostCard
+                        title={post.title}
+                        image={post.image}
+                        content={post.content}
+                        tags={post.tags}
+                    />
                 </div>
-        })
+            )
+        ))
     )
 }
